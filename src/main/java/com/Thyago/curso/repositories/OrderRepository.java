658da5207e9980deb3a13c9a754e0246bfb34255
thyagocantarelli <thyagocantarelli@gmail.com>
2024-06-12 11:44:58 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/Thyago/curso/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.Thyago.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Thyago.curso.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+}
